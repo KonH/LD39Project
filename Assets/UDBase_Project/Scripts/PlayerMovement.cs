5657c333ed57b717b783c488d155406dc9725175
KonH <konh@yandex.ru>
2017-07-29 20:59:06 +0700

    Simple movement;

---
+++ Assets/UDBase_Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace UDBase_Project.Scripts {
+	public class PlayerMovement : MonoBehaviour {
+		public ShipMovement Movement;
+
+		Vector3 FindMoveVector() {
+			return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		}
+
+		Vector3 FindRotateVector() {
+			var mousePos = Input.mousePosition;
+			var w = Screen.width;
+			var h = Screen.height;
+			var x = (mousePos.x - (float) w / 2) / w;
+			var y = (mousePos.y - (float) h / 2) / h;
+			var normalizedMousePos = new Vector2(x, y);
+			return new Vector3(-normalizedMousePos.y, normalizedMousePos.x);
+		}
+		
+		void Update() {
+			Movement.MoveVector = FindMoveVector();
+			Movement.RotateVector += FindRotateVector();
+		}
+	}
+}
---
+++ Assets/UDBase_Project/Scripts/ShipMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace UDBase_Project.Scripts {
+	[RequireComponent(typeof(Rigidbody))]
+	public class ShipMovement : MonoBehaviour {
+		public float MoveSpeed = 1.0f;
+		public float RotateSpeed = 1.0f;
+		
+		[HideInInspector]
+		public Vector3 MoveVector = Vector3.zero;
+		public Vector3 RotateVector = Vector3.zero;
+
+		Rigidbody _rb;
+
+		void Start() {
+			_rb = GetComponent<Rigidbody>();
+		}
+
+		void Update() {
+			_rb.velocity = MoveVector * MoveSpeed;
+			_rb.MoveRotation(Quaternion.Euler(RotateVector * RotateSpeed));
+		}
+	}
+}
