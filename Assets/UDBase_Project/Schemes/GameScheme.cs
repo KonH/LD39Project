84ad448a25c889e6f6f5515f0c3aeb092d9fc5af
KonH <konh@yandex.ru>
2017-07-29 20:32:09 +0700

    UDBase imported;

---
+++ Assets/UDBase_Project/Schemes/GameScheme.cs
@@ -0,0 +1,12 @@
+#if Scheme_GameScheme
+using UnityEngine;
+using System.Collections;
+using UDBase.Common;
+
+public class ProjectScheme : Scheme {
+
+	public ProjectScheme() {
+		// Add your initialization code here
+	}
+}
+#endif
