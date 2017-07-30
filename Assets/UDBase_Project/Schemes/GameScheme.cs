e2f5c1fb5c8dfbfc56f88e3d6f08f139dd744918
KonH <konh@yandex.ru>
2017-07-30 14:40:26 +0700

    Scenes basics;

--- Assets/UDBase_Project/Schemes/GameScheme.cs
+++ Assets/UDBase_Project/Schemes/GameScheme.cs
@@ -1,10 +1,19 @@
 #if Scheme_GameScheme
 using UDBase.Common;
 using UDBase.Controllers.EventSystem;
+using UDBase.Controllers.LogSystem;
+using UDBase.Controllers.SaveSystem;
+using UDBase.Controllers.UserSystem;
 
 public class ProjectScheme : Scheme {
 
 	public ProjectScheme() {
+		AddController<Log>(new UnityLog());
+		
+		var save = new FsJsonDataSave().AddNode<UserSaveNode>("user");
+		AddController<Save>(save);
+		
+		AddController<User>(new SaveUser());
 		AddController<Events>(new EventController());
 	}
 }
---
+++ Assets/UDBase_Project/Scripts/UI/StartUI.cs
@@ -0,0 +1,33 @@
+﻿using UDBase.Controllers.UserSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UDBase_Project.Scripts.UI {
+	public class StartUI : MonoBehaviour {
+
+		public InputField NameInput;
+		public Button StartButton;
+		public Button LeaderboardButton;
+	
+		void Start () {
+			LoadName();
+			NameInput.onValueChanged.AddListener(OnNameChanged);
+			UpdateState();
+		}
+
+		void LoadName() {
+			NameInput.text = User.Name;
+		}
+		
+		void OnNameChanged(string value) {
+			User.Name = value;
+			UpdateState();
+		}
+		
+		void UpdateState() {
+			var isValid = !string.IsNullOrEmpty(User.Name);
+			StartButton.interactable = isValid;
+			LeaderboardButton.interactable = isValid;
+		}
+	}
+}
